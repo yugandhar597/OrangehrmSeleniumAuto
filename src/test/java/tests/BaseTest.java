914b2c483ed1a1079c2ae988b04adbb04b02e46a
A YUGANDHAR <A YUGANDHAR@DESKTOP-MVJ0A5S>
2025-01-17 01:33:33 +0530

    First Push

---
+++ src/test/java/tests/BaseTest.java
@@ -0,0 +1,21 @@
+package tests;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
+
+
+public class BaseTest {
+
+	protected WebDriver driver;
+	@Test
+	public void setup() {
+		driver = new ChromeDriver();
+		String url = "https://opensource-demo.orangehrmlive.com/web/index.php/auth/login";
+		driver.manage().window().maximize();
+		driver.get(url);
+		driver.quit();
+		
+	}
+
+}
